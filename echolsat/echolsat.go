0475bc238d81de4d01d99bf22b9cb9a30e36159a
Dhananjay Purohit <dhananjaypurohit7@gmail.com>
2022-07-30 22:50:31 +0530

    Change package name to package url, Add github actions for running tests (#1)
    * Change package name to package url, Add github actions for running tests
    
    * Update README

--- echolsat/echolsat.go
+++ echolsat/echolsat.go
@@ -6,11 +6,11 @@ import (
 	"fmt"
 	"net/http"
 
-	"echo-lsat/ln"
-	"echo-lsat/lsat"
-	"echo-lsat/macaroon"
-	macaroonutils "echo-lsat/macaroon"
-	"echo-lsat/utils"
+	"github.com/getAlby/echo-lsat/ln"
+	"github.com/getAlby/echo-lsat/lsat"
+	"github.com/getAlby/echo-lsat/macaroon"
+	macaroonutils "github.com/getAlby/echo-lsat/macaroon"
+	"github.com/getAlby/echo-lsat/utils"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
--- echolsat_test.go
+++ echolsat_test.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"testing"
 
-	"echo-lsat/echolsat"
-	"echo-lsat/ln"
+	"github.com/getAlby/echo-lsat/echolsat"
+	"github.com/getAlby/echo-lsat/ln"
 
 	"github.com/labstack/echo/v4"
 
