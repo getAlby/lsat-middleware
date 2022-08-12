100fdf415748e21e7aae1d6681880b6e6415dc08
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2022-08-12 16:31:27 +0530

    Hard-code test macaroon/preimage values

--- ginlsat_test.go
+++ ginlsat_test.go
@@ -25,6 +25,14 @@ const SATS_PER_BTC = 100000000
 
 const MIN_SATS_TO_BE_PAID = 1
 
+const (
+	TEST_MACAROON_VALID = "AgEETFNBVALmAUr/gQMBARJNYWNhcm9vbklkZW50aWZpZXIB/4IAAQMBB1ZlcnNpb24BBgABC1BheW1lbnRIYXNoAf+EAAEHVG9rZW5JZAH/hgAAABT/gwEBAQRIYXNoAf+EAAEGAUAAABn/hQEBAQlbMzJddWludDgB/4YAAQYBQAAAa/+CAiD/pv/jOjY1/9oC/4z/tHb/qf/2Jf+d/4H/u/+YGHj/+/+O/8D/v/+P/8X/qRL/5v/x/4r/tkIBIA1Y/8j/pR3/0P+b/7cwWP+W/87/sD18GP//Hf/f/9Aj//NcBFs2/9VhNEUF/70AAAAGIDlR1jVm5IfEJgvuSQoJLqLg4FcW4Ib1vW8sbkRHdUWX"
+	TEST_PREIMAGE_VALID = "651505fae9ea341c770c6ebef207d8560d546eb3aee26985e584c15d1c987875"
+
+	TEST_MACAROON_INVALID = "AgEETFNBVALhAUr/gQMBARJNYWNhcm9vbklkZW50aWZpZXIB/4IAAQMBB1ZlcnNpb24BBgABC1BheW1lbnRIYXNoAf+EAAEHVG9rZW5JZAH/hgAAABT/gwEBAQRIYXNoAf+EAAEGAUAAABn/hQEBAQlbMzJddWludDgB/4YAAQYBQAAAZv+CAiD/w/+g//R6HUgGQ///DUT/kP+z/7z/wv/9/4AEaX0P/4D/2Gt+/+3/uyEFA0H/8AEg/8f/y0ci/646FkX/vP+YB//kQwH/sUv/wV4KNv+9//Fn/4//7P/WLv/sLP/t/7YxAAAABiB5JhudEoBr8dWuC+BYG4xCl9D90NSwmW8NMw5heQxK+A=="
+	TEST_PREIMAGE_INVALID = "fbe9ac25c04e14b10177514e2d57b0e39224e70277ac1a2cd23c28e58cd4ea35"
+)
+
 func FiatToBTC(currency string, value float64) *http.Request {
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://blockchain.info/tobtc?currency=%s&value=%f", currency, value), nil)
 	if err != nil {
@@ -101,10 +109,6 @@ func TestLsatWithLNURLConfig(t *testing.T) {
 	assert.NoError(t, err)
 
 	LNURL_ADDRESS := os.Getenv("LNURL_ADDRESS")
-	TEST_MACAROON_VALID := os.Getenv("TEST_MACAROON_VALID")
-	TEST_PREIMAGE_VALID := os.Getenv("TEST_PREIMAGE_VALID")
-	TEST_MACAROON_INVALID := os.Getenv("TEST_MACAROON_INVALID")
-	TEST_PREIMAGE_INVALID := os.Getenv("TEST_PREIMAGE_INVALID")
 
 	lnClientConfig := &ln.LNClientConfig{
 		LNClientType: "LNURL",
@@ -184,10 +188,6 @@ func TestLsatWithLNDConfig(t *testing.T) {
 
 	LND_ADDRESS := os.Getenv("LND_ADDRESS")
 	MACAROON_HEX := os.Getenv("MACAROON_HEX")
-	TEST_MACAROON_VALID := os.Getenv("TEST_MACAROON_VALID")
-	TEST_PREIMAGE_VALID := os.Getenv("TEST_PREIMAGE_VALID")
-	TEST_MACAROON_INVALID := os.Getenv("TEST_MACAROON_INVALID")
-	TEST_PREIMAGE_INVALID := os.Getenv("TEST_PREIMAGE_INVALID")
 
 	lnClientConfig := &ln.LNClientConfig{
 		LNClientType: "LND",
